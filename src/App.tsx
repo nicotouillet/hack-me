6bc2a85a6d4ebe258816f76ec42c3083a62efba4
Nicolas Touillet <nicolas@MacBook-Pro-de-Nicolas.local>
2023-01-01 14:43:05 +0100

    adding react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Layout } from './views/Layout'
+import { Desks } from './views/Desks'
+import { Employees } from './views/Employees'
 
-function App() {
-  return <div className="App"></div>
-}
-
-export default App
+export const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="desks" element={<Desks />} />
+          <Route path="employees" element={<Employees />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
+)
